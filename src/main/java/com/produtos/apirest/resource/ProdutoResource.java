d60f63c5a6e86d3861ac7b6f68ffe2244a56d8b2
Patric aLison <Patric aLison>
2021-03-30 19:16:52 -0300

    implementacao da interface produtoRepository

--- src/main/java/com/produtos/apirest/resource/ProdutoResource.java
+++ src/main/java/com/produtos/apirest/resource/ProdutoResource.java
@@ -1,9 +1,13 @@
 package com.produtos.apirest.resource;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +25,20 @@ public class ProdutoResource {
 	public List<Produto> listaProdutos(){
 		
 		return produtoRepository.findAll();
+		
+	}
+	
+	@GetMapping("/produto/{id}")
+	public Optional<Produto> listaProdutoPorId(@PathVariable(value = "id") long id) {
+		
+		return produtoRepository.findById(id);
+		
+	}
+	
+	@PostMapping("/produto")
+	public Produto incluirProduto(@RequestBody Produto produto) {
+		
+		return produtoRepository.save(produto);
 		
 	}
 	
