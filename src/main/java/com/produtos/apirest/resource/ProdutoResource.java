0a4ccc3908f4ae90f81b74440e20a0f796a6bb8e
Patric aLison <Patric aLison>
2021-03-31 12:17:27 -0300

    adicionando Swagger

--- src/main/java/com/produtos/apirest/resource/ProdutoResource.java
+++ src/main/java/com/produtos/apirest/resource/ProdutoResource.java
@@ -1,9 +1,10 @@
 package com.produtos.apirest.resource;
 
 import java.util.List;
-import java.util.Optional;
+//import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,14 +17,20 @@ import org.springframework.web.bind.annotation.RestController;
 import com.produtos.apirest.model.Produto;
 import com.produtos.apirest.repository.ProdutoRepository;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController		//dizendo que aqui é o controll
 @RequestMapping(value="/api")	//uri
+@Api(value="API REST Produtos")		//é o nome que vai aprecer assim que executar
+@CrossOrigin(origins = "*")	//para dar  aturização a api na produção, (origins = "*") um coriga pra todos
 public class ProdutoResource {
 
 	@Autowired
 	ProdutoRepository produtoRepository;
 	
 	@GetMapping("/produto")
+	@ApiOperation(value="Retorna uma lista de produtos")	//serve pra explicar oq essa API faz
 	public List<Produto> listaProdutos(){
 		
 		return produtoRepository.findAll();
@@ -38,6 +45,7 @@ public class ProdutoResource {
 	}*/
 	
 	@GetMapping("/produto/{id}")
+	@ApiOperation(value="Retorna uma lista de produtos pela Id")	//serve pra explicar oq essa API faz
 	public Produto listaProdutoPorId(@PathVariable(value = "id") long id) {
 		
 		return produtoRepository.findById(id);
@@ -45,6 +53,7 @@ public class ProdutoResource {
 	}
 	
 	@PostMapping("/produto")
+	@ApiOperation(value="Inseri um novo produto")	//serve pra explicar oq essa API faz
 	public Produto incluirProduto(@RequestBody Produto produto) {
 		
 		return produtoRepository.save(produto);
@@ -52,6 +61,7 @@ public class ProdutoResource {
 	}
 	
 	@DeleteMapping("/produto")
+	@ApiOperation(value="Deleta um produto do banco")	//serve pra explicar oq essa API faz
 	public void deleteProduto(@RequestBody Produto produto) {
 		
 		produtoRepository.delete(produto);
@@ -73,6 +83,7 @@ public class ProdutoResource {
 		
 	}*/
 	@PutMapping("/produto")
+	@ApiOperation(value="Faz uma atualizacao no produto")	//serve pra explicar oq essa API faz
 	public Produto putProduto(@RequestBody Produto produto) {
 		
 		return produtoRepository.save(produto);
